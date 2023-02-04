7a97a9029554d7ba82aecd82cc11962f6d6aed23
yichangs <44956270+yichangs@users.noreply.github.com>
2023-02-03 23:58:55 -0500

    added the second graph in the second tab panel

--- hw1/app.R
+++ hw1/app.R
@@ -48,6 +48,29 @@ ui <- fluidPage(
                  plotOutput(outputId = "barchart")
                )
              )
+    ),
+    
+    tabPanel("Population for each country", 
+             # Sidebar layout with a input and output definitions --------------
+             sidebarLayout(
+               
+               # Inputs: Select variables to plot ------------------------------
+               sidebarPanel(
+                 
+                 # Select variable for c ----------------------------------
+                 selectInput(inputId = "c", 
+                             label = "Country:",
+                             choices = c("Afghanistan","Argentina", "China", "India", "France", "Germany","Japan", "Mexico", "Philippines", "Portugal", "Republic of Korea",
+                                         "Russian Federation", "Spain", "South Africa", "United Kingdom of Great Britain and Northern Ireland", "United States of America", "Zimbabwe", "Yemen",
+                                         "Ukraine", "Nepal", "Malaysia", "Brazil"), 
+                             selected = "Philippines")
+               ),
+               
+               # Output: Show barplot --------------------------------------
+               mainPanel(
+                 plotOutput(outputId = "country_barchart")
+               )
+             )
     )
   )
 )
@@ -60,6 +83,25 @@ server <- function(input, output) {
     ggplot(data = df, aes_string(x = "country", y = input$y)) + geom_bar(stat = "identity", fill=input$z) +
       theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5)) +
       labs(title=paste0("Population of ", input$y))
+  })
+  
+  population_country <- reactive({
+    new_table <- as.data.frame(t(dplyr::filter(df, country %in% c(input$c))[,-1]))
+    colnames(new_table)[1] ="count"
+    new_table$count_type <- ifelse(new_table$count < 10000000, "below", "above") 
+    new_table$year <- row.names(new_table)
+    new_table
+  })
+  
+  output$country_barchart <- renderPlot({
+    ggplot(data = population_country(), aes(x = year, y = count, label = count)) + 
+      geom_bar(stat='identity', aes(fill=count_type), width=.5)  +
+      scale_fill_manual(name="Count", 
+                        labels = c("Above 80 Million", "Below 80 Millon"), 
+                        values = c("above"="#00ba38", "below"="#f8766d")) + 
+      labs(subtitle=paste0("Population of ", input$c), 
+           title= "Diverging Bars") + 
+      coord_flip()
   })
   
 }
